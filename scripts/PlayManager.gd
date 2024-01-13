36dcf8f1ade0e68707924941f140b0b6d98beacd
unknown <michaelgardone@gmail.com>
2024-01-13 03:13:26 -0700

    game manager now knows how many workers quit

--- scripts/PlayManager.gd
+++ scripts/PlayManager.gd
@@ -4,14 +4,19 @@ signal tick_update
 
 @export var tickUpdateTimer:float = 0.5
 @export var quarterTimer:float = 60
+@export var quitFailPercent:float = 0.6
 
 @onready var tickTimer = $TickUpdateTimer
 
 var quarterlyMoneyCounter:int = 0
 
+var total_qrtr_workers:int = 0
+var total_workers_quit:int = 0
+
 func _ready():
 	tickTimer.start(tickUpdateTimer)
 	SignalBus.connect("give_player_money", _give_player_money_receiver)
+	SignalBus.connect("worker_quit", _worker_quit)
 ##
 
 func _on_tick_update_timer_timeout():
@@ -26,4 +31,13 @@ func _give_player_money_receiver(money:int):
 
 func _on_quarter_timer_timeout():
 	pass # Replace with function body.
+##
+
+func _worker_quit():
+	total_workers_quit += 1
+	
+	# if this worker quiting exceeds the threshold, fail the player
+	if total_workers_quit / total_qrtr_workers > quitFailPercent:
+		pass
+	##
 ##
--- scripts/SignalBus.gd
+++ scripts/SignalBus.gd
@@ -6,5 +6,7 @@ signal worker_quit
 signal worker_asleep
 signal worker_awake
 
+# DICKHEAD SIGNALS
+
 # ENIVRONMENT SIGNALS
 signal player_attacked_dickhead
