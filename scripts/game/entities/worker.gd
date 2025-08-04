2edb4458a697de6402dd5352516ba1e4ea5120cf
Mica Gardone <michaelgardone@gmail.com>
2025-08-03 23:52:00 -0600

    fixing bug by resetting a value, swapping variable names to make sense with UI for convenience's sake

--- scripts/game/entities/worker.gd
+++ scripts/game/entities/worker.gd
@@ -7,10 +7,10 @@ signal worker_quits(worker:Worker)
 @export var EnergyDecreasePerTick:float = -2
 @export var TemperatureIncreasePerTick:float = 2
 
-var _temperature:float = 60
+var _stress:float = 0
+var _temperature:float = 50
 var _cooling_down:bool = false
 var _overheating:bool = false
-var _energy_level:float = 50
 var _multiplier:float = 1
 var _energy_change:float
 var _is_stressed:bool = false
@@ -21,40 +21,40 @@ func _ready():
 ##
 
 func _check_health():
-	if _energy_level <= 0:
+	if _temperature <= 0:
 		if _multiplier != 0:
 			$ArtWorker.be_asleep()
 			_multiplier = 0
 		##
-		_energy_level = 0
-	elif _energy_level < 33:
+		_temperature = 0
+	elif _temperature < 33:
 		if _multiplier != 0.5:
 			$ArtWorker.be_happy()
 			_multiplier = 0.5
 		##
-	elif _energy_level < 66:
+	elif _temperature < 66:
 		if _multiplier != 1:
 			$ArtWorker.be_meh()
 			_multiplier = 1
 		##
 	else:
 		if _overheating and _cooling_down == false:
-			_temperature = min(100, _temperature + (
+			_stress = min(100, _stress + (
 					TemperatureIncreasePerTick  *
 						(1.0 if _is_affected_by_manager == false else 1.25)
 				)
 			)
-			if _temperature >= 100:
+			if _stress >= 100:
 				# TODO: Quit animation
 				worker_quits.emit(self)
 				queue_free() # << Replace this
 			##
 		##
 		
-		if _temperature < 51 and _is_stressed == false:
+		if _stress < 51 and _is_stressed == false:
 			_is_stressed = true
 			$ArtWorker.be_stressed()
-		elif _temperature > 50 and _is_stressed:
+		elif _stress > 50 and _is_stressed:
 			_is_stressed = false
 			$ArtWorker.be_onfire()
 		##
@@ -79,23 +79,23 @@ func increase_energy(amnt:float):
 
 func change_energy():
 	if _is_affected_by_manager:
-		_energy_level += 100 #-_energy_change * 1.5
+		_temperature += 100 #-_energy_change * 1.5
 		$ArtWorker.set_temperature(100)
 	else:
-		_energy_level += _energy_change
+		_temperature += _energy_change
 		$ArtWorker.add_to_temperature(_energy_change)
 	##
 	
-	if _overheating and _energy_level < 66:
-		_energy_level = 66
+	if _overheating and _temperature < 66:
+		_temperature = 66
 	##
 	
-	if _energy_level > 100:
-		_energy_level = 100
+	if _temperature > 100:
+		_temperature = 100
 	##
 	
-	if _energy_level < 0:
-		_energy_level = 0
+	if _temperature < 0:
+		_temperature = 0
 	##
 	
 	_energy_change = EnergyDecreasePerTick
@@ -119,18 +119,18 @@ func cooloff(decrease:float):
 
 func payoff(decrease:float):
 	if _is_affected_by_manager == false:
-		_temperature += decrease
+		_stress += decrease
 	##
 ##
 
 func is_quitting():
-	return _temperature >= 100
+	return _stress >= 100
 ##
 
 func affected_by_dickhead():
 	_is_affected_by_manager = true
-	if _energy_level < 25:
-		_energy_level = 25
+	if _temperature < 25:
+		_temperature = 25
 	##
 	# Force the energy to be it's usual value
 	_energy_change = EnergyDecreasePerTick
